794729f5ef0591672139f5edb6ca748023f4929c
Vladimir Lidzhigoryaev <vlidzhigoryaev@digital-spirit.ru>
2025-03-25 12:15:37 +0300

    apply spotless checks

--- src/main/java/org/itmo/testing/lab2/Application.java
+++ src/main/java/org/itmo/testing/lab2/Application.java
@@ -5,11 +5,10 @@ import org.itmo.testing.lab2.controller.UserAnalyticsController;
 
 public class Application {
 
-    private static final int DEFAULT_PORT = 8080;
+  private static final int DEFAULT_PORT = 8080;
 
-    public static void main(String[] args) {
-        Javalin app = UserAnalyticsController.createApp();
-        app.start(DEFAULT_PORT);
-    }
-
+  public static void main(String[] args) {
+    Javalin app = UserAnalyticsController.createApp();
+    app.start(DEFAULT_PORT);
+  }
 }
